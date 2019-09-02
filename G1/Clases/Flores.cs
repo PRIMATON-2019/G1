66e6f1972500992c6e5f462bfe515aa391baa2f4
facucalle92 <54385211+facucalle92@users.noreply.github.com>
2019-09-02 20:10:19 -0300

    Add files via upload

---
+++ G1/Clases/Flores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    class Flores
+    {
+        public String nombre { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ G1/Clases/Semillas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Semillas
+    {
+        public String nombre { get; set; }
+        public int id { get; set; }
+        public String tipo { get; set; }
+
+    }
+}
