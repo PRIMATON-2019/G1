ee03f834b2050827d312ffac48ac984beab77130
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-09-03 20:45:55 -0300

    Ordenando carpetas

---
+++ G1/Clases/Modelos/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1.Clases.Modelos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string NombreProducto { get; set; }
+        // El tipo estaria dado por un indice, ej: 1- Arbol / 2 - Semilla / 3 - Fruto
+        public int TipoProducto { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ G1/Clases/Modelos/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace G1.Clases
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public Persona Persona { get; set; }
+        public int Rol { get; set; }
+    }
+}
